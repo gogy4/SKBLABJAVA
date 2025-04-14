4eb4faa203c25345a7b3a3dc795bcbff5a7deebe
Данил <younggogy@gmail.com>
2025-04-14 19:22:21 +0500

    сделал конфиги

---
+++ src/main/java/com/springurfu/configHomeWork/beans/TestBean.java
@@ -0,0 +1,10 @@
+﻿package com.springurfu.configHomeWork.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TestBean {
+    private final String message;
+}
---
+++ src/main/java/com/springurfu/configHomeWork/config/ExampleTestNotDefaultCondition.java
@@ -0,0 +1,16 @@
+﻿package com.springurfu.configHomeWork.config;
+
+import lombok.var;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class ExampleTestNotDefaultCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        var env = context.getEnvironment();
+        var value = env.getProperty("EXAMPLE_TEST");
+        return value != null && !value.equalsIgnoreCase("default");
+    }
+}
